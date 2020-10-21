614a4c81a750d19094acfd574e64f4fd01e5b9f0
Michael Rose <mikescottrose@gmail.com>
2020-10-21 15:11:34 -0700

    Clean up duty
    Removed vars and reformatted

--- index.js
+++ index.js
@@ -25,30 +25,28 @@ app.use(express.static(path.join(__dirname, 'views')));
 const router = express.Router();
 
 //Routing for /newton
-router.get('/newton', function (request, response, next) {
+router.get('/newton', function(request, response, next) {
     const someError = false;
     if (someError) {
         return next(someError);
     }
     //Requests data from omdb
-    var request = new XMLHttpRequest()
-    request.open('GET', 'https://www.omdbapi.com/?apikey='+CONFIG.api_key+'&s=newton', false)
-    request.send();
+    let req = new XMLHttpRequest()
+    req.open('GET', 'https://www.omdbapi.com/?apikey=' + CONFIG.api_key + '&s=newton', false)
+    req.send();
     //Converts request to JSON
-    console.log(request.responseText);
-    var json = JSON.parse(request.responseText);
+    const json = JSON.parse(req.responseText);
     //Strips any excess JSON or returns and error if an unexpected JSON is returned.
     if (json.hasOwnProperty("Search")) {
         response.send(json["Search"]);
     } else {
         const err = new Error('OMDb did not return valid data.');
-        response.send('We seem to have encountered some sort of E double R O R. Please be patient while we resolve the problem. ' + err)
+        response.status(418).send('We seem to have encountered some sort of E double R O R. Please be patient while we resolve the problem. ' + err)
     }
-
 });
 
 //Home. This is where our story begins…
-router.get('/', function (req, res, _next) {
+router.get('/', function(req, res, _next) {
     res.sendFile('index.html', {
         root: './views'
     })
@@ -57,25 +55,25 @@ app.use('/', router);
 
 //Set the port
 server.listen(process.env.PORT || 9003);
-server.on('error', function (err) {
+server.on('error', function(err) {
     console.log('"Err" on the side of caution. Error found:', err)
 });
-server.on('listening', function (lst) {
+server.on('listening', function(lst) {
     console.log('Hello, hello. Is anybody out there? I\'m here waiting!')
 });
-server.on('request', function (req) {
+server.on('request', function(req) {
     //Uncomment this if you want see EVERY incoming request.
     //But let's be real, this is going to spam the logs on such a simple app.
-    // console.log(req.url);
+    //console.log(req.url);
 })
 
 //Error handling. Didn't find what you were looking for huh?
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     const err = new Error('Page Not Found');
     err.status = 404;
     next(err);
 });
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.send('We seem to have encountered some sort of E double R O R. Please be patient while we resolve the problem. ' + err)
 });
